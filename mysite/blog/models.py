b6bb89ef340aa280f78fc5934e515640311124c4
Amstl86 <39667704+Amstl86@users.noreply.github.com>
2025-07-10 13:17:09 +0700

    Модефицирован метод get_absolute_url  в модели Post.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -25,7 +25,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     body = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE,
@@ -53,5 +54,8 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse(
             'blog:post_detail',
-            args=[self.id]
+            args=[self.publish.year,
+                  self.publish.month,
+                  self.publish.day,
+                  self.slug]
         )
