dbb5dd93d05b29c1bf2ff6ef5cc291aabacb7204
Amstl86 <39667704+Amstl86@users.noreply.github.com>
2025-07-23 17:15:04 +0700

    Добавлено поле тегов в модель поста и подключен пакет taggit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.conf import settings
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -41,6 +42,7 @@ class Post(models.Model):
     
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    tags = TaggableManager()
     
     class Meta:
         ordering = ['-publish']
